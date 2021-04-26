c4c4b764502fd867ed56f306fac12fa32eeb7a14
Roldie <rdavideliseo15@gmail.com>
2021-04-25 19:10:35 -0700

    cambio hecho por ROLDAN

---
+++ Movimiento2David.py
@@ -0,0 +1,7 @@
+print("Un soldado a casa hoy regresó"
+"Y un niño enfermo se curó"
+"Y hoy no hay trabajo en el bosque de la lluvia"
+"Un desamparado se salvó"
+"Por causa de una buena acción"
+"Y hoy nadie lo repudia, Aleluya"
+"Aleluya, Aleluya, Aleluya, Aleluya")
