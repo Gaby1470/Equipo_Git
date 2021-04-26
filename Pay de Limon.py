22644ab5bdade26f6b6b96193cae43bfa211cf26
Gaby <gabriela.sanchez.alcaraz@gmail.com>
2021-04-25 23:34:35 -0700

    Receta para un pay de limon facil pero rico

---
+++ Pay de Limon.py
@@ -0,0 +1,4 @@
+print("INGREDIENTES:\n-8 limones\n-1 lechera\n-1 carnation\n-galletas marias\nPREPARACION:\n-Licuar"
+      "la lechera,carnation y el jugo de ocho limones\n-En un bowl poner una capa de galletas marias y una capa fina"
+      "de la mezcla, luego otra capa de galletas y otra capa fina de galletas (repetir hasta que se acabe la mezcla)"
+      "\n-dejarlo enfriar")
